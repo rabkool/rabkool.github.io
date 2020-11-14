b76b809ee8a0a37f372a32b7ddd45dfd2ab3bcf0
rabkool <905956347@qq.com>
2020-11-14 23:58:31 +0900

    1

---
+++ config.js
@@ -0,0 +1,20 @@
+window.config = {
+  server: 'https://api.github.com',
+  siteName: 'rabkool',
+  navbar: [
+    {
+      label: '首页',
+      link: '/'
+    }, {
+      label: '博客',
+      link: '/blog'
+    }, {
+      label: 'react',
+      link: '/blog/tag/react'
+    }
+  ],
+  github: {
+    user: 'rabkool',
+    repo: 'blog'
+  }
+}
